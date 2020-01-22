fd044b08edd423b46f20324eb32eb056738111db
karthik <karthik261099@gmail.com>
2020-01-22 21:29:08 +0530

    Created server to android server code

---
+++ serverToAndroidCode.php
@@ -0,0 +1,22 @@
+<?php
+
+$response['error']=true;
+$response['message']="Message";
+$response['phone']="+919029192655";
+
+$fp = fsockopen("192.168.137.76", 7881, $errno, $errstr, 3);
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    fwrite($fp, json_encode($response));
+
+    // while (!feof($fp)) {
+    //     echo fgets($fp, 128);
+    // }
+
+
+    fclose($fp);
+}
+
+
+?>
