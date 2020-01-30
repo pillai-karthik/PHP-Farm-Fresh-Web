19968aceba3b1c0d80857ed08c322b9a4e6543d5
karthik <karthik261099@gmail.com>
2020-01-30 19:24:55 +0530

    Read Me file created for the flow of setting up the project

--- serverToAndroidCode.php
+++ serverToAndroidCode.php
@@ -4,7 +4,7 @@ $response['error']=true;
 $response['message']="Message";
 $response['phone']="+919029192655";
 
-$fp = fsockopen("192.168.137.76", 7881, $errno, $errstr, 3);
+$fp = fsockopen("192.168.0.25", 7881, $errno, $errstr, 3);
 if (!$fp) {
     echo "$errstr ($errno)<br />\n";
 } else {
